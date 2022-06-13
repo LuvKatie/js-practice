bc8c9a29c24bd597d6d7e745d769ef54dc0049d2
Tony <bobacrypto2@gmail.com>
2022-06-13 02:51:41 -0700

    Understand split function for array and for of loops

--- script.js
+++ script.js
@@ -126,3 +126,31 @@
 //             console.log(i)
 //         }
 // }
+
+
+// contact.split(":") here is splitting each object in the array by the separator :
+// thus for (const contact of contacts) each object is being sperated into its own array of splitContact each time
+// resulting in storing the values on split of : so e.g - Chris ":" 2232322
+// and this will reiterate each time until the input matches splitContact
+
+// const contacts = ['Chris:2232322', 'Sarah:3453456', 'Bill:7654322', 'Mary:9998769', 'Dianne:9384975'];
+// const para = document.querySelector('p');
+// const input = document.querySelector('input');
+// const btn = document.querySelector('button');
+
+// btn.addEventListener('click', () => {
+//   const searchName = input.value.toLowerCase();
+//   input.value = '';
+//   input.focus();
+//   para.textContent = '';
+//   for (const contact of contacts) {
+//     const splitContact = contact.split(':');
+//     if (splitContact[0].toLowerCase() === searchName) {
+//       para.textContent = splitContact[0] + '\'s number is ' + splitContact[1] + '.';
+//       break;
+//     }
+//   }
+//   if (para.textContent === '') {
+//    para.textContent = 'Contact not found.';
+//  }
+// });
